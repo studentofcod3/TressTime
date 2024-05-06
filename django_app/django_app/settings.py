b6c999860daf3e3679221d9ae05dd0fc5c2c00ec
Ayyoub Maknassa <ayyoub.maknassa@hotmail.com>
2024-05-07 00:22:53 +0100

    Add auto import of modules in shell with shell_plus
    command: 'python manage.py shell_plus'

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Other apps
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
