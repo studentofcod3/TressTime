e41d88f8e4ca842b31165288b61b73eca1aadf72
Ayyoub Maknassa <ayyoub.maknassa@hotmail.com>
2024-05-29 22:58:28 +0100

    Convert tests to use mocks instead DB access, increase coverage

--- django_app/users/repositories/user_repository.py
+++ django_app/users/repositories/user_repository.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 
 from users.models import CustomUser
 from .interfaces.user_repository_interface import UserRepositoryInterface
 
 
 class UserRepository(UserRepositoryInterface):
-    # TODO: Test for exceptions
     def get_user_by_id(self, user_id):
         try:
             return CustomUser.objects.get(id=user_id)
@@ -16,8 +16,11 @@ class UserRepository(UserRepositoryInterface):
         return CustomUser.objects.all()
     
     def create_user(self, user_data):
-        # TODO: Add try catch in case missing data and test for this
-        return CustomUser.objects.create(**user_data)
+        try:
+            return CustomUser.objects.create(**user_data)
+        except IntegrityError:
+            return None
+
     
     def update_user(self, user_id, user_data):
         try:
@@ -28,8 +31,7 @@ class UserRepository(UserRepositoryInterface):
             return user
         except CustomUser.DoesNotExist:
             return None
-        except:
-            # TODO: Find error for when updates with non-existent field
+        except IntegrityError:
             return None
     
     def delete_user(self, user_id):
--- django_app/users/tests/test_repositories.py
+++ django_app/users/tests/test_repositories.py
@@ -1,89 +1,117 @@
-
 import pytest
+
+from django.db import IntegrityError
+from unittest.mock import patch, MagicMock
+
 from users.repositories.user_repository import UserRepository
 from users.models import CustomUser
 
 
-@pytest.mark.django_db
 class TestUserRepository:
-    # TODO: Docstrings
-    # TODO: Non happy paths
 
-    user_repository = UserRepository()
+    @pytest.fixture
+    def mock_user(self):
+        """A mock object for the CustomUser model."""
+        return MagicMock(spec=CustomUser)
 
-    username='test_username'
-    username_2='test_username_2'
-    email='test_email'
-    email_2='test_email_2'
-    password='test_password'
-    password_2='test_password_2'
+    @pytest.fixture
+    def user_repository(self):
+        """An instance of UserRepository."""
+        return UserRepository()
     
-    def create_user(self):
-        return CustomUser.objects.create(
-            username=self.username,
-            email=self.email,
-            password=self.password
-        )
+    def test_get_user_by_id(self, mock_user, user_repository):
+        """Ensures get_user_by_id calls User.objects.get with the correct parameters and returns the mock user."""
+        user_id = 'some-unique-id'
+        with patch.object(CustomUser.objects, 'get', return_value=mock_user) as mock_get:
+            user = user_repository.get_user_by_id(user_id)
+            mock_get.assert_called_once_with(id=user_id)
+            assert user == mock_user
+
+    def test_get_user_by_id_not_found(self, user_repository):
+        """Ensures get_user_by_id handles User.DoesNotExist correctly."""
+        user_id = 'non-existent-id'
+        with patch.object(CustomUser.objects, 'get', side_effect=CustomUser.DoesNotExist):
+            user = user_repository.get_user_by_id(user_id)
+            assert user is None
+        
+    def test_get_all_users(self, mock_user, user_repository):
+        """Checks that get_all_users calls the 'all' method exactly once and returns the expected list of users."""
+        mock_users = [mock_user, mock_user]
+        with patch.object(CustomUser.objects, 'all', return_value=mock_users) as mock_all:
+            users = user_repository.get_all_users()
+            mock_all.assert_called_once()
+            assert users == mock_users
+
+    def test_create_user(self, mock_user, user_repository):
+        """Ensures create_user calls User.objects.create with the correct parameters and returns the mock user."""
+        user_data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'testpassword'
+        }
+        with patch.object(CustomUser.objects, 'create', return_value=mock_user) as mock_create:
+            user = user_repository.create_user(user_data)
+            mock_create.assert_called_once_with(**user_data)
+            assert user == mock_user
     
-    def create_second_user(self):
-        return CustomUser.objects.create(
-            username=self.username_2,
-            email=self.email_2,
-            password=self.password_2
-        )
-    
-    def test_get_user_id(self):
-        custom_user = self.create_user()
-        
-        user = self.user_repository.get_user_by_id(custom_user.id)
-        assert user.username == self.username
-        assert user.email == self.email
-        assert user.password == self.password
-        
+    def test_create_user_invalid_data(self, user_repository):
+        """Ensures that create_user handles IntegrityError correctly."""
+        user_data = {
+            'username': 'testuser',
+            'email': None,  # This should trigger an IntegrityError due to not null constraint
+            'password': 'testpassword'
+        }
+        with patch.object(CustomUser.objects, 'create', side_effect=IntegrityError) as mock_create:
+            user = user_repository.create_user(user_data)
+            mock_create.assert_called_once_with(**user_data)
+            assert not user
 
-    def test_get_all_users(self):
-        custom_user = self.create_user()
-        custom_user_2 = self.create_second_user()
+    def test_update_user(self, mock_user, user_repository):
+        """Ensures update_user updates the user and saves it."""
+        user_id = 'some-unique-id'
+        user_data = {
+            'email': 'newemail@example.com'
+        }
+        assert mock_user.email != user_data["email"]
+        with patch.object(CustomUser.objects, 'get', return_value=mock_user):
+            updated_user = user_repository.update_user(user_id, user_data)
+            for key, value in user_data.items():
+                setattr(mock_user, key, value)
+            mock_user.save.assert_called_once()
+            assert updated_user == mock_user
+            assert mock_user.email == user_data["email"]
 
-        all_users = self.user_repository.get_all_users()
+    def test_update_user_with_invalid_data(self, user_repository):
+        """Ensures that update_user handles IntegrityError correctly."""
+        user_id = 'some-unique-id'
+        invalid_user_data = {
+            'email': 'invalid-email',  # Assume this is an invalid email format
+        }
+        with patch.object(CustomUser.objects, 'get', side_effect=IntegrityError):
+            user = user_repository.update_user(user_id, invalid_user_data)
+            assert not user
 
-        assert len(all_users) == 2
-        assert custom_user in all_users
-        assert custom_user_2 in all_users
+    def test_update_user_not_found(self, user_repository):
+        """Ensures update_user handles User.DoesNotExist correctly."""
+        user_id = 'non-existent-id'
+        user_data = {
+            'email': 'newemail@example.com'
+        }
+        with patch.object(CustomUser.objects, 'get', side_effect=CustomUser.DoesNotExist):
+            updated_user = user_repository.update_user(user_id, user_data)
+            assert updated_user is None
 
+    def test_delete_user(self, mock_user, user_repository):
+        """Ensures delete_user deletes the user and returns True."""
+        user_id = 'some-unique-id'
+        with patch.object(CustomUser.objects, 'get', return_value=mock_user):
+            result = user_repository.delete_user(user_id)
+            mock_user.delete.assert_called_once()
+            assert result is True
 
-    def test_create_user(self):
-        new_user = self.user_repository.create_user(
-            user_data={
-                'username': self.username,
-                'email': self.email,
-                'password':self.password,
-            }
-        )
-        assert new_user.username == self.username
-        assert new_user.email == self.email
-        assert new_user.password == self.password
-
-    def test_update_user(self):
-        custom_user = self.create_user()
-
-        updated_user = self.user_repository.update_user(
-            user_id=custom_user.id,
-            user_data={
-                'username': self.username_2,
-            }
-        )
-        assert updated_user.id == custom_user.id
-        assert updated_user.username == self.username_2
-        # Other fields should remain the same
-        assert updated_user.email == self.email
-        assert updated_user.password == self.password
-
-    def test_delete_user(self):
-        custom_user = self.create_user()
-        assert len(self.user_repository.get_all_users()) == 1
-        
-        self.user_repository.delete_user(
-            user_id=custom_user.id
-        )
-        assert len(self.user_repository.get_all_users()) == 0
+    def test_delete_user_not_found(self, user_repository):
+        """Ensures delete_user handles User.DoesNotExist correctly and returns False."""
+        user_id = 'non-existent-id'
+        with patch.object(CustomUser.objects, 'get', side_effect=CustomUser.DoesNotExist):
+            result = user_repository.delete_user(user_id)
+            assert result is False
